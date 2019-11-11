22d3e827a458e7dce178b3eddf006c05d9233468
adkhiker <poirier.greg@gmail.com>
2019-11-10 22:14:43 -0500

    Added testing

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,8 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+//import PlayerList from "./components/PlayerList";
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import { waitForElement } from "@testing-library/react";
+
+afterEach(rtl.cleanup);
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+test("Player Detection", async () => {
+  const wrapper = rtl.render(<App />);
+  const player = await waitForElement(() =>
+    wrapper.queryByText(/ellen white/i)
+  );
+  expect(player).toBeTruthy();
 });
